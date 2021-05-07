31ad7eb02e408059f7a31f2a822f3cfeb2d24680
Abdelkarim Azzouguagh <abdel.azzouguagh@gmail.com>
2021-05-07 16:56:26 +0200

    ej5 rice

--- project/src/com/multimedia_tech/FramesManager.java
+++ project/src/com/multimedia_tech/FramesManager.java
@@ -72,7 +72,7 @@ public class FramesManager {
      * */
     public void saveImagesToZip(String fname) throws IOException{
 
-        FileOutputStream fileOS = new FileOutputStream(fname + ".aor");
+        FileOutputStream fileOS = new FileOutputStream(fname + ".zip");
         ZipOutputStream zipOS = new ZipOutputStream(fileOS);
         // Para cada imagen, creamos un fichero jpeg temporal,
         // el cual se añadirá al archivo zip mediante la función createFileToZip
